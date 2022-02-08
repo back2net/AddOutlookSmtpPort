2880efbbe1c449e6eb89c8110cac42d13fea8450
amfetamin <amfetamin@2net.su>
2022-02-08 08:51:22 +0300

    add support for outlook 2019

--- app.go
+++ app.go
@@ -8,24 +8,101 @@ import (
 )
 
 const (
+	OUTLOOKSETTINGS = `SOFTWARE\Microsoft\Office`
 	OUTLOOKPROFILES = `SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows Messaging Subsystem\Profiles`
 	SETTINGS        = `9375CFF0413111d3B88A00104B2A6676`
 )
 
 func main() {
+	setSmtpAddressInProfile()
+	setSmtpAddressInSettings()
+}
+
+func setSmtpAddress(s registry.Key) {
+	var smtpKey string
+	smtpTemp, valType, _ := s.GetBinaryValue("SMTP Server")
+	if valType == 1 {
+		smtpKey, _, _ = s.GetStringValue("SMTP Server")
+	} else {
+		smtpKey = strings.Replace(string(smtpTemp), "\x00", "", -1)
+	}
+	if smtpKey == "exchange.svetlana-k.ru" {
+		err := s.SetDWordValue("SMTP Port", 587)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func getSubkeys(k registry.Key) (subKeys []string, err error) {
+	subKeys, err = k.ReadSubKeyNames(-1)
+	if err != nil {
+		log.Println(err)
+	}
+	return subKeys, err
+}
+
+func setSmtpAddressInSettings() {
+	k, err := registry.OpenKey(
+		registry.CURRENT_USER,
+		OUTLOOKSETTINGS,
+		registry.ENUMERATE_SUB_KEYS)
+
+	if err != nil {
+		log.Println(err)
+	}
+	defer k.Close()
+
+	versions, err := getSubkeys(k)
+	if err != nil {
+		log.Println(err)
+
+	}
+
+	for _, version := range versions {
+		versionSettings, err := registry.OpenKey(
+			registry.CURRENT_USER,
+			OUTLOOKSETTINGS+"\\"+version+"\\"+"Outlook\\Profiles\\Outlook"+"\\"+SETTINGS,
+			registry.READ)
+		if err != nil {
+			log.Println(err)
+		}
+		defer k.Close()
+
+		sk, err := getSubkeys(versionSettings)
+		if err != nil {
+			log.Println(err)
+		}
+
+		for _, v := range sk {
+			settings, err := registry.OpenKey(
+				registry.CURRENT_USER,
+				OUTLOOKSETTINGS+"\\"+version+"\\"+"Outlook\\Profiles\\Outlook"+"\\"+SETTINGS+"\\"+v,
+				registry.ALL_ACCESS)
+			if err != nil {
+				log.Println(err)
+			}
+			defer settings.Close()
+			setSmtpAddress(settings)
+
+		}
+	}
+}
+
+func setSmtpAddressInProfile() {
 	k, err := registry.OpenKey(
 		registry.CURRENT_USER,
 		OUTLOOKPROFILES,
 		registry.ENUMERATE_SUB_KEYS)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 	defer k.Close()
 
 	profiles, err := getSubkeys(k)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 
 	for _, profileName := range profiles {
@@ -34,48 +111,26 @@ func main() {
 			OUTLOOKPROFILES+"\\"+profileName+"\\"+SETTINGS,
 			registry.READ)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
 		defer k.Close()
-		setSmtpAddress(profileSettings, profileName)
 
-	}
-
-}
-
-func setSmtpAddress(ps registry.Key, pn string) {
-	sk, err := getSubkeys(ps)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, v := range sk {
-		settings, err := registry.OpenKey(
-			registry.CURRENT_USER,
-			OUTLOOKPROFILES+"\\"+pn+"\\"+SETTINGS+"\\"+v,
-			registry.ALL_ACCESS)
+		sk, err := getSubkeys(profileSettings)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
-		defer settings.Close()
 
-		smtpTemp, _, _ := settings.GetBinaryValue("SMTP Server")
-
-		smtpKey := strings.Replace(string(smtpTemp), "\x00", "", -1)
-
-		if smtpKey == "exchange.svetlana-k.ru" {
-			err = settings.SetDWordValue("SMTP Port", 587)
+		for _, v := range sk {
+			settings, err := registry.OpenKey(
+				registry.CURRENT_USER,
+				OUTLOOKPROFILES+"\\"+profileName+"\\"+SETTINGS+"\\"+v,
+				registry.ALL_ACCESS)
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
 			}
+			defer settings.Close()
+			setSmtpAddress(settings)
+
 		}
 	}
-}
-
-func getSubkeys(k registry.Key) (subKeys []string, err error) {
-	subKeys, err = k.ReadSubKeyNames(-1)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return subKeys, err
 }
